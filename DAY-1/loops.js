5734d28e775c4fd60185ea2c8808667058983a1d
rafey155 <rafeymohd155@gmail.com>
2026-05-09 01:23:35 +0530

    learning conditional statements

---
+++ DAY-1/loops.js
@@ -0,0 +1,51 @@
+// for loop 
+
+// print hello 5 times
+let i = 1;
+for (i; i<=5; i++){
+    console.log("hello")
+}
+
+// print number form 1 to 5
+let j = 1
+for (j; i<= 5; i++){
+    console.log(i)
+}
+
+// print the number for 10 to 1 in reverse order
+for ( let i = 10; i>=1; i--){
+    console.log(i)
+}
+
+// print the table of 5 
+for (i = 1; i <=10; i++){
+    console.log(i*5)
+}
+
+// printing table using litrals \\
+for (let i = 1; i<=10; i++){
+    console.log(`5 X ${i} = ${i*5}`)
+}
+
+// while loop
+// print hello 5 times 
+let num = 1;
+while(num <=5){
+    console.log("hello");
+    num++
+}
+
+// print the number for 0 to 10 
+let a = 1;
+while (a <=10){
+    console.log(a)
+    a++
+}
+
+// print the reverse counting form 10 to 1 
+let b = 10;
+while(b>=1){
+    console.log(b)
+    b--
+}
+
---
+++ DAY-1/read.md
@@ -0,0 +1,3 @@
+git add .           
+git commit -m "learning conditional statements" 
+git push origin main
