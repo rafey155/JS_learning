ef04d39656eef372457ee0557f1337291eb12ba0
rafey155 <rafeymohd155@gmail.com>
2026-05-08 23:13:59 +0530

    learning conditional statements

---
+++ DAY-1/conditional.js
@@ -0,0 +1,33 @@
+// if statement 
+let num = 34;
+if(num == 34){
+    console.log("thirty four");
+}
+
+// if-else statement 
+let age = 19 
+if (age >= 18){
+    console.log("you can vote")
+}
+else{
+    console.log("Can not vote")
+}
+
+// If-else-if Statement
+let weight = 45;
+if(weight == 50){
+    console.log("FIt")
+}
+else if(weight > 50){
+    console.log("do some pushups")
+}
+else {
+    console.log("est something")
+}
+
+// template litreals
+let string1 = "Main pal do pal ka shayr hoon"
+let string2 = "pal do pal meri khani hai"
+let multiLineLiterals = `${string1}, 
+${string2}`
+console.log(multiLineLiterals)
