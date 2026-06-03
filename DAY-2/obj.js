fb7d0cbe42fd2e279f756ab730626ec61bc28951
rafey155 <rafeymohd155@gmail.com>
2026-06-03 23:15:41 +0530

    learinig day 3

---
+++ DAY-2/obj.js
@@ -0,0 +1,14 @@
+const loginObj = {
+    name : "Rafey",
+    age : 23,
+    course : "Btech CSE",
+    greet : (name) =>{
+        console.log(`Hello !${name}`)
+    }
+}
+
+console.log(loginObj)
+loginObj.greet("Rafey")
+
+// let arr = [1,2,3,4,5,6,7];
+// console.log(arr[3])
